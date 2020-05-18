3c7dac5f51dcc45c5a4e2da7937fd10cb003716c
Janbee-Shaik <janbee.shaik05@gmail.com>
2020-05-18 11:26:23 +0530

    First Commit

---
+++ src/test/java/com/hcl/ospoc/OcpsspServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hcl.ospoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OcpsspServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
